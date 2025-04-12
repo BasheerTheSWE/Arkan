8f2e4567a328af9fbde2c1a3c1311c2b043c4014
Basheer-dev <basheer0abdulmalik@gmail.com>
2025-04-12 05:38:38 +0300

    init

---
+++ Arkan/ArkanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArkanApp.swift
+//  Arkan
+//
+//  Created by Basheer Abdulmalik on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ArkanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
