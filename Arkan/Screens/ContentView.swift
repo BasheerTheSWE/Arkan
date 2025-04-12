a16f300901f6d80da8dd2b28e16639d16935625d
Basheer-dev <basheer0abdulmalik@gmail.com>
2025-04-12 09:05:51 +0300

    Menu design + ui refresh

--- Arkan/Screens/ContentView.swift
+++ Arkan/Screens/ContentView.swift
@@ -10,37 +10,82 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                PlainIconButton(systemImage: "square.and.arrow.up.fill") {
+            MainHeaderView()
+            
+            VStack {
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                VStack(spacing: 45) {
+                    VStack {
+                        Text("Al Taif, SA")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                        
+                        Text("Umm al-Qura University, Makkah")
+                            .font(.system(size: 12, design: .rounded))
+                            .foregroundStyle(.secondary)
+                    }
                     
+                    HStack {
+                        Text("12, April 2025")
+                        
+                        Spacer()
+                        
+                        Text("13, Shawwal 1446")
+                    }
+                    .font(.system(size: 12, weight: .medium, design: .monospaced))
+                    .padding(.horizontal)
                 }
+                
+                Divider()
                 
                 Spacer()
                 
-                Text("بسم الله الرحمن الرحيم")
-                    .foregroundStyle(.white)
+                VStack {
+                    ForEach(0..<5) { _ in
+                        HStack {
+                            Image(systemName: "sunrise")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 18, height: 18)
+                            
+                            Text("Fajr")
+                                .font(.system(size: 16, weight: .medium, design: .rounded))
+                                .padding(.leading)
+                            
+                            Spacer()
+                            
+                            Text("4:43 AM")
+                                .font(.system(size: 14, weight: .medium, design: .monospaced))
+                                .foregroundStyle(.secondary)
+                            
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "speaker.wave.1.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundStyle(Color(.label))
+                                    .frame(width: 16, height: 16)
+                            }
+                            .padding(.leading)
+                        }
+                        .padding()
+                        .background(Color(.secondarySystemGroupedBackground))
+                        .clipShape(.rect(cornerRadius: 8))
+                    }
+                }
                 
                 Spacer()
-                
-                PlainIconButton(systemImage: "line.3.horizontal") {
-                    
-                }
+                Spacer()
+                Spacer()
             }
             .padding()
-            .padding(.bottom, 12)
-            .foregroundStyle(.white)
-            .frame(maxWidth: .infinity)
-            .background(.black)
-            .overlay(alignment: .bottom) {
-                RoundedRectangle(cornerRadius: .infinity)
-                    .fill(Color(.secondarySystemGroupedBackground))
-                    .frame(height: 16)
-                    .offset(y: 8)
-            }
-            
-            Spacer()
+            .frame(maxHeight: .infinity)
         }
-        .background(Color(.secondarySystemGroupedBackground))
+        .background(Color(.systemGroupedBackground))
     }
 }
 
---
+++ Arkan/Views/Others/MainHeaderView.swift
@@ -0,0 +1,89 @@
+//
+//  MainHeaderView.swift
+//  Arkan
+//
+//  Created by Basheer Abdulmalik on 12/04/2025.
+//
+
+import SwiftUI
+
+struct MainHeaderView: View {
+    
+    @Environment(\.colorScheme) private var colorScheme
+    @State private var isShowingMenuItems = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                PlainIconButton(systemImage: "square.and.arrow.up.fill") {
+                    
+                }
+                
+                Spacer()
+                
+                Text("بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ")
+                    .foregroundStyle(.white)
+                
+                Spacer()
+                
+                PlainIconButton(systemImage: "line.3.horizontal") {
+                    withAnimation { isShowingMenuItems.toggle() }
+                }
+            }
+            
+            if isShowingMenuItems {
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        Text("About Us")
+                            .font(.system(size: 14, weight: .bold, design: .rounded))
+                            .frame(height: 44)
+                            .frame(maxWidth: .infinity)
+                            .background(.white.opacity(0.07))
+                            .clipShape(.rect(cornerRadius: 8))
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Contact Us")
+                            .font(.system(size: 14, weight: .bold, design: .rounded))
+                            .frame(height: 44)
+                            .frame(maxWidth: .infinity)
+                            .background(.white.opacity(0.07))
+                            .clipShape(.rect(cornerRadius: 8))
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Settings")
+                            .font(.system(size: 14, weight: .bold, design: .rounded))
+                            .frame(height: 44)
+                            .frame(maxWidth: .infinity)
+                            .background(.white.opacity(0.07))
+                            .clipShape(.rect(cornerRadius: 8))
+                    }
+                }
+                .padding(.top)
+                .transition(.scale.combined(with: .opacity).combined(with: .blurReplace))
+            }
+        }
+        .padding()
+        .padding(.bottom, 12)
+        .foregroundStyle(.white)
+        .frame(maxWidth: .infinity)
+        .background(colorScheme == .light ? .black : Color(.secondarySystemBackground))
+        .overlay(alignment: .bottom) {
+            RoundedRectangle(cornerRadius: .infinity)
+                .fill(Color(.systemGroupedBackground))
+                .frame(height: 16)
+                .offset(y: 8)
+        }
+    }
+}
+
+#Preview {
+    MainHeaderView()
+}
