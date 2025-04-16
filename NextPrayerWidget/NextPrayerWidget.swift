dbeb2d56f5c8f1f39be3599801876abe0aeee1f1
Basheer-dev <basheer0abdulmalik@gmail.com>
2025-04-16 16:24:44 +0300

    Redesigned the circular next prayer widget

--- NextPrayerWidget/NextPrayerWidget.swift
+++ NextPrayerWidget/NextPrayerWidget.swift
@@ -178,6 +178,77 @@ struct NextPrayerWidgetEntryView : View {
     }
 }
 
+struct NextPrayerTimeCircularWidgetView: View {
+    
+    private let systemImage: String
+    private let progress: CGFloat
+    
+    private let entry: Provider.Entry
+    
+    // MARK: - INIT
+    init(entry: Provider.Entry) {
+        self.entry = entry
+        
+        let images = [
+            "sunrise",
+            "sun.max",
+            "cloud.sun",
+            "sunset",
+            "moon"
+        ]
+        
+        self.systemImage = images[Prayer.allCases.firstIndex(of: entry.nextPrayer) ?? 0]
+        self.progress = CGFloat(Prayer.allCases.firstIndex(of: entry.nextPrayer) ?? 0) / 4.0
+    }
+    
+    // MARK: - VIEW
+    var body: some View {
+        Text(getTimeString())
+            .font(.system(size: 24, weight: .bold))
+            .lineLimit(1)
+            .scaledToFit()
+            .minimumScaleFactor(0.2)
+            .padding(.horizontal, 12)
+            .padding(.bottom, 4)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background {
+                ZStack {
+                    Circle()
+                        .trim(from: 0.0, to: 0.8)
+                        .stroke(Color.white, style: StrokeStyle(lineWidth: 6, lineCap: .round))
+                        .rotationEffect(.degrees(126))
+                    
+                    Circle()
+                        .trim(from: 0.0, to: 0.001)
+                        .stroke(Color.black, style: StrokeStyle(lineWidth: 12, lineCap: .round))
+                        .rotationEffect(.degrees(126.0 + (286 * progress)))
+                }
+                .compositingGroup()
+                .luminanceToAlpha()
+                .overlay {
+                    Circle()
+                        .trim(from: 0.0, to: 0.001)
+                        .stroke(Color(.label), style: StrokeStyle(lineWidth: 7, lineCap: .round))
+                        .rotationEffect(.degrees(126.0 + (286 * progress)))
+                }
+            }
+            .overlay(alignment: .bottom) {
+                Image(systemName: systemImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 16, height: 16)
+            }
+    }
+    
+    private func getTimeString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        formatter.timeZone = .current
+        return formatter.string(from: entry.nextPrayerDate)
+    }
+}
+
+
 struct NextPrayerTimeMediumWidgetView: View {
     
     private let systemImage: String
@@ -284,9 +355,9 @@ struct NextPrayerWidget: Widget {
     }
 }
 
-#Preview(as: .systemMedium) {
+#Preview(as: .accessoryCircular) {
     NextPrayerWidget()
 } timeline: {
-    NextPrayerTimeEntry(date: .now, nextPrayerDate: Calendar.current.date(byAdding: .hour, value: 2, to: .now) ?? .now, prayer: .isha)
-    NextPrayerTimeEntry(date: Calendar.current.date(byAdding: .hour, value: 2, to: .now) ?? .now, nextPrayerDate: Calendar.current.date(byAdding: .hour, value: 4, to: .now) ?? .now, prayer: .dhuhr)
+    NextPrayerTimeEntry(date: .now, nextPrayerDate: Calendar.current.date(byAdding: .hour, value: 2, to: .now) ?? .now, prayer: .asr)
+//    NextPrayerTimeEntry(date: Calendar.current.date(byAdding: .hour, value: 2, to: .now) ?? .now, nextPrayerDate: Calendar.current.date(byAdding: .hour, value: 4, to: .now) ?? .now, prayer: .dhuhr)
 }
